397246dccad0f02b02902c924051964471228650
YadavDipak <yadavdipak2911@gmail.com>
2025-02-20 13:21:38 +0530

    New Releases Song

---
+++ components/album/NewReleases.jsx
@@ -0,0 +1,48 @@
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import ReleaseCard from "./ReleaseCard";
+import { getNewReleases } from "../../services/album";
+
+const NewReleases = () => {
+  const [newReleases, setNewReleases] = useState([]);
+
+  useEffect(() => {
+    const fetchNewReleases = async () => {
+      try {
+        const result = await getNewReleases();
+        if (result) {
+          setNewReleases(result);
+        }
+      } catch (err) {}
+    };
+
+    fetchNewReleases();
+  }, []);
+
+  return (
+    <>
+      <View style={{ height: 10 }} />
+      <Text
+        style={{
+          color: "white",
+          fontSize: 19,
+          fontWeight: "bold",
+          marginHorizontal: 8,
+          marginTop: 8,
+        }}
+      >
+        New Releases
+      </Text>
+      <FlatList
+        data={newReleases}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item, index }) => (
+          <ReleaseCard item={item} key={index} />
+        )}
+      />
+    </>
+  );
+};
+
+export default NewReleases;
