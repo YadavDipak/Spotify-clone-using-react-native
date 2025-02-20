783cddc8b44c89489a9d7e672f605041fe85043f
YadavDipak <yadavdipak2911@gmail.com>
2025-02-20 13:17:46 +0530

    Song Info Screen

---
+++ screens/SongInfo.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SongInfo = () => {
+  return (
+    <View>
+      <Text>SongInfo</Text>
+    </View>
+  );
+};
+
+export default SongInfo;
+
+const styles = StyleSheet.create({});
