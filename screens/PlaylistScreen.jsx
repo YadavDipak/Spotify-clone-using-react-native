75986c36ea800df3fa5f5719477fac740cb40126
YadavDipak <yadavdipak2911@gmail.com>
2025-02-20 13:15:02 +0530

    Playlist Screen

---
+++ screens/PlaylistScreen.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const PlaylistScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>PlaylistScreen ------------------</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default PlaylistScreen;
+
+const styles = StyleSheet.create({});
