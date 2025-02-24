0a94cd28f33ce00f7be2ea45d07b48a7cab10997
YadavDipak <yadavdipak2911@gmail.com>
2025-02-24 14:50:25 +0530

    Recently Played songs

--- components/RecentlyPlayedCard.jsx
+++ components/RecentlyPlayedCard.jsx
@@ -1,12 +1,17 @@
 import { Text, Pressable, Image, View } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const RecentlyPlayedCard = ({ item }) => {
+  const navigation = useNavigation();
   return (
-    <Pressable style={{ margin: 10 }}>
+    <Pressable
+      style={{ margin: 10 }}
+      onPress={() => navigation.navigate("SongInfo", { item: item.track })}
+    >
       <Image
         style={{ width: 130, height: 130, borderRadius: 5 }}
-        source={{ uri: item.track.album.images[0].url }}
+        source={{ uri: item?.track?.album?.images?.[0]?.url }}
       />
       <View>
         <Text
