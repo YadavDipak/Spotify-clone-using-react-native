7006ef745646be9291238f489d55e0b77d57d36f
YadavDipak <yadavdipak2911@gmail.com>
2025-03-06 09:48:19 +0530

    Added

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
+// import React, { useEffect } from "react";
+// import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { store, persistor } from "./src/redux/store";
+// import Navigation from "./StackNavigator";
+
+// const RootLayout = () => {
+//   return (
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <Navigation />
+//       </PersistGate>
+//     </Provider>
+//   );
+// };
+
+// export default RootLayout;
+
 import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./src/redux/store";
 import Navigation from "./StackNavigator";
+import SplashScreen from "react-native-splash-screen";
 
 const RootLayout = () => {
+  useEffect(() => {
+    const hideSplash = async () => {
+      // Ensure SplashScreen is not null before calling hide()
+      if (SplashScreen && SplashScreen.hide) {
+        SplashScreen.hide();
+      }
+    };
+
+    setTimeout(hideSplash, 500); // Add a slight delay for stability
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
